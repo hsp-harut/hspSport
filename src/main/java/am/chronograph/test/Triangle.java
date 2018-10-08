f9176177e595a9c4828f0c51f18081fecc77c696
Harut Sargsyan <Harutyun_Sargsyan1@epam.com>
2018-10-08 14:22:36 +0400

    Removinf init/destroy methods

--- src/main/java/am/chronograph/test/Triangle.java
+++ src/main/java/am/chronograph/test/Triangle.java
@@ -2,14 +2,6 @@ package am.chronograph.test;
 
 public class Triangle implements Shape {
 	
-	public void init() {
-		System.out.println("Init method called!");
-	}
-	
-	public void dest() {
-		System.out.println("Dest method called!");
-	}
-
 	@Override
 	public void draw() {
 		System.out.println("Drawing a Triangle");
