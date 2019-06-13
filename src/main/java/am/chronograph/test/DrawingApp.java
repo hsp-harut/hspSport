789791e5ad44d8da6f52e5b2a4e68bc44e8d4aa8
Lusine Hunanyan <lusine.hunanyan@hsp.am>
2019-06-13 18:00:41 +0400

    testing

--- src/main/java/am/chronograph/test/DrawingApp.java
+++ src/main/java/am/chronograph/test/DrawingApp.java
@@ -1,25 +1,30 @@
 package am.chronograph.test;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.FileSystemResource;
 
 public class DrawingApp {
 
 	public static void main(String[] args) {
-//		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("/home/harut/projects/chronograph/src/main/resources/testSpringBeans.xml"));
-//		Triangle triangle = (Triangle) factory.getBean("triangle");
+		/*BeanFactory factory = new XmlBeanFactory(new FileSystemResource("/home/harut/projects/chronograph/src/main/resources/testSpringBeans.xml"));
+		Triangle triangle = (Triangle) factory.getBean("triangle");
+	
+		triangle.draw();*/
 		
-		ApplicationContext context = new ClassPathXmlApplicationContext("testSpringBeans.xml");		
+		//ApplicationContext context = new ClassPathXmlApplicationContext("testSpringBeans.xml");		
 		
-//		AbstractApplicationContext context = new ClassPathXmlApplicationContext("testSpringBeans.xml");
-//		context.registerShutdownHook();
+		/*AbstractApplicationContext context = new ClassPathXmlApplicationContext("testSpringBeans.xml");
+		context.registerShutdownHook();
 		
 		Triangle triangle = (Triangle) context.getBean("triangle");
 		
+		triangle.draw();*/
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("testSpringBeans.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
 		triangle.draw();
+		
+		
+		
 	}
 }
--- src/main/java/am/chronograph/test/Triangle.java
+++ src/main/java/am/chronograph/test/Triangle.java
@@ -1,10 +1,33 @@
 package am.chronograph.test;
 
 public class Triangle implements Shape {
+	private String type;
+	private int height;
+	
+ public Triangle(String type) {
+		this.type = type;
+	}
+ 
+ public Triangle(String type, int height) {
+		this.type = type;
+		this.height = height;
+	}
 	
 	@Override
 	public void draw() {
-		System.out.println("Drawing a Triangle");
+		System.out.println(getType() + " Drawing a Triangle");
+	}
+	
+	/*public void setType(String type) {
+		this.type = type;
+	}*/
+	
+	public String getType() {
+		return type;
+	}
+	
+	public int getHeight() {
+		return height;
 	}
 
 }
